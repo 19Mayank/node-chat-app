c58147c4db8be9d0d49b93471cc384461cb73f08
Mayank Parashar <19.Mayank@gmail.com>
2018-09-06 13:19:42 +0530

    Add CSS for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -31,14 +31,16 @@
       jQuery('#messages').append(li);
     });
 
+    var messageTextBox = jQuery('[name=message]');
+
   jQuery('#message-form').on('submit', function (e) {
     e.preventDefault()
 
     socket.emit('createMessage', {
-      from:'User',
-      text:jQuery('[name=message]').val()
+      from: 'User',
+      text: messageTextBox.val()
     }, function() {
-
+      messageTextBox.val('')
     });
   });
 
@@ -48,12 +50,16 @@
       return alert('Geolocation not supported by your browser.')
     }
 
+    locationButton.attr('disabled', 'disabled').text('sending location...');
+
     navigator.geolocation.getCurrentPosition(function (position){
+      locationButton.removeAttr('disabled').text('send location');
       socket.emit('createLocationMessage', {
         latitude: position.coords.latitude,
         longitude: position.coords.longitude
       });
     }, function (){
+      locationButton.removeAttr('disabled').text('send location');
       alert('Unable to fetch Location.');
     });
   });

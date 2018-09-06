97efaba10b815f51f805e584f7e1008a6870bcfa
Mayank Parashar <19.Mayank@gmail.com>
2018-09-06 21:51:46 +0530

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,23 @@
 
   var socket = io();
 
+  function scrollToBottom(){
+
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child');
+
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if(clientHeight + scrollTop +newMessageHeight + lastMessageHeight >= scrollHeight){
+      messages.scrollTop(scrollHeight);
+    }
+
+  }
+
   socket.on('connect', function(){
     console.log('connected to server.');
   });
@@ -22,6 +39,7 @@
 
     jQuery('#messages').append(html);
 
+    scrollToBottom();
     });
 
     socket.on('newLocationMessage', function(message) {
@@ -38,6 +56,7 @@
         });
 
         jQuery('#messages').append(html);
+        scrollToBottom();
     });
 
     var messageTextBox = jQuery('[name=message]');

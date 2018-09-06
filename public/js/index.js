afae8efb54317bf76276df11aee1ed078cbe0b61
Mayank Parashar <19.Mayank@gmail.com>
2018-09-06 10:14:05 +0530

    Add from for messages and show incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -10,11 +10,23 @@
     console.log('Disconnected from server.');
   });
 
-
   socket.on('newMessage', (message) => {
       console.log('new Message',message);
+
+      var li = jQuery('<li></li>');
+      li.text(`${message.from}: ${message.text}`);
+
+      jQuery('#messages').append(li);
+
     });
 
-  socket.on('welcome', (message) => {
-        console.log(message);
+  jQuery('#message-form').on('submit', function (e) {
+    e.preventDefault()
+
+    socket.emit('createMessage', {
+      from:'User',
+      text:jQuery('[name=message]').val()
+    }, function() {
+
     });
+  });
